acd1314c09d8ddcb2087ed865b534d661440f5e9
OAndreae <oliverandreae@outlook.com>
2022-07-01 13:05:35 +0100

    Added documentation to ship types.

--- Battleships.Engine/Ship.cs
+++ Battleships.Engine/Ship.cs
@@ -6,6 +6,13 @@ namespace Battleships.Engine;
 public record Ship {
     public int Size { get; init;}
 
+    /// <summary>
+    /// A Ship of size 4.
+    /// </summary>
     public static Ship Destroyer = new Ship { Size = 4 };
+    
+    /// <summary>
+    /// A ship of size 5.
+    /// </summary>
     public static Ship Battleship = new Ship { Size = 5 };
 }
